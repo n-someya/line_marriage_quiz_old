55d40dea165671e590288cce3df0ad68c7789417
Someya Naoki <n.someya.ynu@gmail.com>
2017-10-17 23:28:54 +0900

    Temporary change

---
+++ QuizManager.js
@@ -0,0 +1,22 @@
+'use strict';
+const { Client } = require('pg');
+
+class QuizManager {
+
+    constructor() {
+        this.client = new Client();
+    }
+
+    get_current_stage() {
+        return pg_client.connect()
+            .then(res => {
+                //現在の設問番号を取得
+                return pg_client.query('select coalesce((select max(stage)  from corrects), 0) + 1;')
+            }).then(row => {
+                return Promise.resolve({
+                    type: 'text',
+                    text: "現在の問題番号は " + row[0] + " です。"
+                });
+            })
+    }
+}
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const line = require('@line/bot-sdk');
 const express = require('express');
-const { Client } = require('pg');
+const QuizManager = require('./QuizManager')
 
 // create LINE SDK config from env variables
 const config = {
@@ -20,16 +20,6 @@ const app = express();
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/webhook', line.middleware(config), (req, res) => {
-  console.log(req.body);
-  // 
-  //   .then(res => {
-  //     pg_client.query('select * from answers;')
-  //       .then(res => {
-  //         console.log("DB connect ok: ", res);
-  //       }).catch(e => {
-  //         console.log("DB connect ng: ", e.stack);
-  //       });
-  //   });
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result));
@@ -37,48 +27,40 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 // event handler
 function handleEvent(event) {
-  const pg_client = new Client();
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
+  const quiz_manager = QuizManager();
   if (event.message.text === "ref") {
-    console.log("ref: comming !!!!!!");
-    return pg_client.connect()
-      .then(res => {
-        pg_client.query('select * from answers;')
-          .then(res => {
-            const pgquery = { type: 'text', text: JSON.stringify(res) };
-            console.log("DB connect ok: ", res);
-            return client.replyMessage(event.replyToken, pgquery);
-          }).catch(e => {
-            const pgquery = { type: 'text', text: e.stack };
-            console.log("DB connect ng: ", e.stack);
-            return client.replyMessage(event.replyToken, pgquery);
-          })
-      });
-  }else{
-    return pg_client.connect()
-    .then(res =>{
-      //現在の設問番号を取得
-      pg_client.query('select coalesce((select max(stage)  from corrects), 0) + 1;')
-      .then(res => {
-        const pgquery = {
-          type: 'text',
-          text: "現在の問題番号は " + res + " です。" 
-        };
-        return client.replyMessage(event.replyToken, pgquery);
+    // return pg_client.connect()
+    //   .then(res => {
+    //     pg_client.query('select * from answers;')
+    //       .then(res => {
+    //         const pgquery = { type: 'text', text: JSON.stringify(res) };
+    //         console.log("DB connect ok: ", res);
+    //         return client.replyMessage(event.replyToken, pgquery);
+    //       }).catch(e => {
+    //         const pgquery = { type: 'text', text: e.stack };
+    //         console.log("DB connect ng: ", e.stack);
+    //         return client.replyMessage(event.replyToken, pgquery);
+    //       })
+    // });
+  } else {
+    quiz_manager.get_current_stage()
+      .then(message => {
+        return client.replyMessage(event.replyToken, message);
       })
-    })
+      .catch(e => {
+        //TODO If error has occured, shoud return sorry message
+        return client.replyMessage(event.replyToken, e.message);
+      });
   }
-  const text = event.message.text + "イカ？";
+  // const text = event.message.text + "イカ？";
   // create a echoing text message
-  const echo = { type: 'text', text: text };
-  console.log("userid=")
-  console.log(event.source.userId)
-
+  // const echo = { type: 'text', text: text };
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  // return client.replyMessage(event.replyToken, echo);
 }
 
 // listen on port
